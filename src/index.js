a7aa33e6244e17ba1a76fa57c178b164bea75861
Jeff Lobacz <jeff.lobacz@gmail.com>
2022-05-15 17:11:10 -0700

    add in our sign in component as well as firebase utils and brought in firebase libraries

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// The following line is commented out as it is no longer needed with React 18?
+// import ReactDOM from "react-dom";
+
+// The following line was added to use the new root API
+import ReactDOMClient from "react-dom/client";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// The 2 following lines were added to used the new root API
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+
+// ReactDOM was replaced with 'root' to use the new root API
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
+  // The following line was commented out as it appears to be redundant
+  // document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
